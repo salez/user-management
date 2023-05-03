933ac3caa7092fed924cdaf26e1a08bbdbae03d3
salez <r.sales.s@gmail.com>
2023-05-03 17:15:29 -0300

    feat: add login components

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,17 +1,60 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '@core/auth/auth.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '@core/auth/services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent {
+  protected showPassword: boolean = false;
+  public loginForm: FormGroup;
+
   constructor(
-    private authService: AuthService
-  ){}
+    private authService: AuthService,
+    private fb: FormBuilder
+  ) { 
+    this.loginForm = this.fb.group({
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      password: ['', [
+        Validators.required,
+      ]]
+    });
+  }
+
+  login(){
+    this.authService.fakeLoginWithInsecurePassValidation({
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value
+    }).subscribe((user) => {
+
+    },
+    (error) => {
+
+    });
+  }
 }
